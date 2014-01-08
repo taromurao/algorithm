50e66c3e948d9627fe9a681a8507ad44eec71fe7
Taro Murao <taro.murao@gmail.com>
2014-01-08 15:15:16 +0100

    Test implemented for binary-sort.

--- test/algorithm/sort_test.clj
+++ test/algorithm/sort_test.clj
@@ -1,8 +1,13 @@
 (ns algorithm.sort-test
   (:require [clojure.test :refer :all]
-            [algorithm.sort :refer :all])
-  (:use [clojure.string :as str]))
+            [algorithm.sort :refer :all]))
 
-(deftest numerical-vector
-  (is (= [1 1 2 3] (merge-sort [2 1 3 1]))))
+(defn check [f]
+  (is (= [1 1 2 3] (f [2 1 3 1]))))
+
+(deftest test-merge-sort
+  (check merge-sort))
+
+(deftest test-binary-sort
+  (check binary-sort))
 
