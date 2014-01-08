425c4fe0aca0f88e0a8da1839ae624fd28190f9c
Taro Murao <taro.murao@gmail.com>
2014-01-08 16:27:25 +0100

    fib analysis result is added as a comment.

--- src/algorithm/core.clj
+++ src/algorithm/core.clj
@@ -1,10 +1,12 @@
 (ns algorithm.core
   (:gen-class)
   (:require [algorithm.sort :refer :all]
-            [algorithm.sample-data :as data])
+            [algorithm.sample-data :as data]
+            [algorithm.fibonacci :refer :all])
   (:use criterium.core))
 
 (defn -main []
   ;(with-progress-reporting (bench (merge-sort data/int-vector))))
-  (with-progress-reporting (bench (binary-sort data/int-vector))))
+  ;(with-progress-reporting (bench (binary-sort data/int-vector))))
+  (with-progress-reporting (bench (fib 20))))
 
